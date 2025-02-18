e3036712382826f5185654681a68a36ba6a689f2
RolandSamidu <samidu.appuhamy@gmail.com>
2025-02-18 23:25:14 +0530

    Added UserService.java

--- src/main/java/com/hospitalmanagementsystem/healthcare/service/UserService.java
+++ src/main/java/com/hospitalmanagementsystem/healthcare/service/UserService.java
@@ -1,4 +1,30 @@
 package com.hospitalmanagementsystem.healthcare.service;
 
+import com.hospitalmanagementsystem.healthcare.model.User;
+import com.hospitalmanagementsystem.healthcare.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUserById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
