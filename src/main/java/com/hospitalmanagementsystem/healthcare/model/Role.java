9e74648aaf37aff214823f251e466350ea6bff1d
RolandSamidu <samidu.appuhamy@gmail.com>
2025-02-18 16:51:23 +0530

    Updated User.java and add Role.java

---
+++ src/main/java/com/hospitalmanagementsystem/healthcare/model/Role.java
@@ -0,0 +1,17 @@
+package com.hospitalmanagementsystem.healthcare.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String name;
+}
--- src/main/java/com/hospitalmanagementsystem/healthcare/model/User.java
+++ src/main/java/com/hospitalmanagementsystem/healthcare/model/User.java
@@ -2,20 +2,28 @@ package com.hospitalmanagementsystem.healthcare.model;
 
 import jakarta.persistence.*;
 import lombok.*;
+import java.util.Set;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(unique = true, nullable = false)
-    private String email;
+    private String username;
 
+    @Column(nullable = false)
     private String password;
-    private String userRole;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
 }
